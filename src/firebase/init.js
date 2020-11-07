555cd8cfea30f7f8b24c1bd5f131a2a9bd7515c5
Ahmad Satiri <satiri.a@gmail.com>
2020-11-07 16:55:50 +0700

    remove firebase config from repo

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase'
-// import firestore from 'firebase/firestore'
 
+// update this from local firebaseConfig.js
 const firebaseConfig = {
-    apiKey: "AIzaSyBlWBUg_qp66UMeQC1qBzxe6_1MgZMgEXk",
-    authDomain: "data-warga-b8625.firebaseapp.com",
-    databaseURL: "https://data-warga-b8625.firebaseio.com",
-    projectId: "data-warga-b8625",
-    storageBucket: "data-warga-b8625.appspot.com",
-    messagingSenderId: "1040772955208",
-    appId: "1:1040772955208:web:88d75ad6eab7cd88f2ec7d"
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
